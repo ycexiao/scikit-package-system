f4229e5d35822f0add437db26b4c9b1d61e2568a
Sangjoon Bob Lee <bobleesj@gmail.com>
2025-04-16 22:16:35 -0400

    skpkg: add cookiecutter for system level

---
+++ {{ cookiecutter.github_repo_name }}/src/{{ cookiecutter.package_dir_name }}/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# All rights reserved.
+#
+#
+# See LICENSE.rst for license information.
+#
+##############################################################################
+
+# package version
+from {{cookiecutter.package_dir_name}}.version import __version__
+
+# silence the pyflakes syntax checker
+assert __version__ or True
+
+# End of file
