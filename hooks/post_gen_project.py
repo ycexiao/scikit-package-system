486441e059b12b77b0fccf6cdeb7696d58237733
Sangjoon Bob Lee <bobleesj@gmail.com>
2025-04-18 23:08:31 -0400

    chore: remove isort since it conflicts with black

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,15 +1,17 @@
 def main():
     """Execute when user runs cookiecutter."""
 
-    print(f"""
+    print(
+        f"""
     Congratulations! A new Python project is created!
     Enter the directory with `cd {{cookiecutter.project_name}}`.
     Run `pip install -e .` to install the package in editable mode.
     To ensure the package is installed correctly, run `pytest`
 
     For more info, please refer to the official documentation on Level 4 of sharing.
-    """)
+    """
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- {{ cookiecutter.github_repo_name }}/tests/test_calculator.py
+++ {{ cookiecutter.github_repo_name }}/tests/test_calculator.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pytest
-
 from {{cookiecutter.package_dir_name}} import calculator as calc
 
 
